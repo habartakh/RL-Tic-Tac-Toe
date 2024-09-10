9662f32c44bb81e2acb542e405da38a4bbe2a6fe
Hajar BARTAKH <bartakh.hajar@gmail.com>
2024-09-11 00:36:44 +0200

    adjusted game_board structure into a list

--- main.py
+++ main.py
@@ -21,18 +21,18 @@ class TicTacToe:
                 self.buttons[i][j].grid(row=i, column=j)
                 
         # Game information 
-        self.current_player = "x"
+        self.current_player = 1
+        self.current_player_symbol = "x"
         self.game_set  = False
-        self.game_board = [[" " for _ in range(3)] for _ in range(3)] # will be filled with "x" or "o"
+        self.game_board = [0 for _ in range(9)] # will be filled with "x" or "o"
 
         
     # when clicking on a button, fill it with the appropriate text 
     def on_button_click(self, row, col):
-        if(self.game_board[row][col] == " "):
-            self.buttons[row][col].configure(text = self.current_player)
-            self.game_board[row][col] = self.current_player
-            
-       
+        if(self.game_board[row*3+col] == 0):
+            self.buttons[row][col].configure(text = self.current_player_symbol)
+            self.game_board[row*3+col] = self.current_player
+              
         if self.check_winner() :
             if self.show_winner_message() : # Player clicked on YES button
                 self.reset_game() # Play another round 
@@ -49,49 +49,53 @@ class TicTacToe:
             self.switch_player() 
         
         
+    
     def check_winner(self):
-        for row in self.game_board : 
-            if row[0] == row[1] == row[2] != " " :
-                return True
+       for i in range(3):
+           if self.game_board[i*3] == self.game_board[i*3+1] == self.game_board[i*3+2] != 0 :
+               return self.game_board[i*3]
         
-        for col in range (3):
-            if self.game_board[0][col] == self.game_board[1][col] == self.game_board[2][col] != " " :
-                return True 
+           if self.game_board[i] ==self.game_board[3+i] == self.game_board[6+i] != 0 :
+               return self.game_board[i]
         
+       if self.game_board[4] == self.game_board[6] == self.game_board[2] and self.game_board[4] != 0 : 
+            return  self.game_board[4]
         
-        if (self.game_board[0][0] == self.game_board[1][1] == self.game_board[2][2] != " "
-        or self.game_board[0][2] == self.game_board[1][1] == self.game_board[2][0] != " "):
-            return True 
+       elif self.game_board[0] == self.game_board[4] == self.game_board[8] and self.game_board[0] != 0  :
+            return self.game_board[0] 
         
-        return False
+       return False
         
+    
     def show_winner_message(self):
-        return messagebox.askyesno(None, "Player " + self.current_player + " won. Do you wish to play another round?")
+        return messagebox.askyesno(None, "Player " + self.current_player_symbol + " won. Do you wish to play another round?")
       
     def show_draw_message(self) : 
         return messagebox.askyesno(None, "The game ended in a draw. Wanna try your luck again?")
         
     def check_draw(self):
-        if any(" " in row for row in self.game_board) == False and self.check_winner()==False :
+        if (0 in self.game_board) == False and self.check_winner() == False :
             return True
         return False 
         
     def switch_player(self):
-       # print ("Inside switch player method !")
-        if self.current_player == "x" : 
+        if self.current_player == 1 : 
             # print ("current player is X. Switching to O")
-            self.current_player = "o"
+            self.current_player = 2
+            self.current_player_symbol = "o"
         
         else :
            # print ("current player is O. Switching to X")
-            self.current_player = "x"
+            self.current_player = 1
+            self.current_player_symbol = "x"
                 
     def reset_game(self):
        for i in range (3):
            for j in range (3) : 
                self.buttons[i][j].configure(text = " ")
-               self.game_board[i][j] = " "
-       self.current_player = "x"
+               self.game_board[i*3+j] = 0
+       self.current_player = 1
+       self.current_player_symbol ="x"
                
                
     def run_game(self):
